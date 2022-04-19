6657896d39b45f313834f4df2529154421188fd3
Owen Halliday <35672537+drekdrek@users.noreply.github.com>
2022-04-19 09:27:24 -0400

    add negative number support

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/square/<int:num>')
+@app.route('/square/<num>')
 def square(num):
+    num = int(num)
     return str(num**2)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
+
 
