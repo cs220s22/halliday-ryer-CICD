e32435ca7b2dd4a8442971d88285b68fd243308a
Owen Halliday <35672537+drekdrek@users.noreply.github.com>
2022-04-21 10:33:30 -0400

    added docstrings

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
+"""A Flask application that serves as a simple web calculator"""
+
 from flask import Flask
 app = Flask(__name__)
 
-"""A Flask application that serves as a simple web calculator
-"""
+
 
 @app.route('/square/<num>')
 def square(num):
-    """This function takes a number and returns the square of it"""
+    """
+    This function takes a number and returns the square of it
+    """
 
     num = int(num)
     return str(num**2)
